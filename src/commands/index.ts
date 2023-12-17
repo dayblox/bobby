979068bac94edb7ac4ab698f60e799a6d281666b
Dayblox <tanstack@gmail.com>
2023-12-17 23:27:53 +0100

    add shuffle

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,5 @@
+import coinflip from "./coinflip"
 import random from "./random"
+import shuffle from "./shuffle"
 
-export default [random]
+export default [random, coinflip, shuffle]
---
+++ src/commands/shuffle.ts
@@ -0,0 +1,23 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js"
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("shuffle")
+		.setDescription("Shuffle words randomly")
+		.addStringOption((option) =>
+			option
+				.setName("words")
+				.setDescription("Words to shuffle")
+				.setRequired(true),
+		),
+	async execute(int: ChatInputCommandInteraction) {
+		const words = int.options.getString("words")!.split(/[ ,;_/-]+/)
+		for (let i = words.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1))
+			const temp = words[i]
+			words[i] = words[j]
+			words[j] = temp
+		}
+		int.reply(words.join(" "))
+	},
+}
