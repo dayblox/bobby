8147eed208542e4c74c73a57ddac3a5ae7d48d8c
Dayblox <tanstack@gmail.com>
2023-12-19 03:32:46 +0100

    add log

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,18 @@ register(commands.map(({ data }) => data))
 new Client({ intents: [GatewayIntentBits.Guilds] })
 	.on("interactionCreate", (interaction) => {
 		if (!interaction.isChatInputCommand()) return
+		console.log(
+			JSON.stringify({
+				command: interaction.commandName,
+				options: interaction.options.data[0].options?.map(
+					({ name, value, user }) => ({
+						name,
+						value: user?.displayName ?? value,
+					}),
+				),
+				author: interaction.user.displayName,
+			}),
+		)
 		commands
 			.find(({ data: { name } }) => name === interaction.commandName)
 			?.execute(interaction)
