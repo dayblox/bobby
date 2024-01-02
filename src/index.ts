3563f6ba8676bf40915c457ac30e8862bc397b6c
Dayblox <tanstack@gmail.com>
2024-01-02 22:03:18 +0100

    clarify guard clause

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ register(commands.map(({ data }) => data))
 
 new Client({ intents: [GatewayIntentBits.Guilds] })
 	.on("interactionCreate", (interaction) => {
-		if (!interaction.isChatInputCommand()) return
-		commands
-			.find(({ data: { name } }) => name === interaction.commandName)
-			?.execute(interaction) && log(interaction)
+		if (interaction.isChatInputCommand())
+			commands
+				.find(({ data: { name } }) => name === interaction.commandName)
+				?.execute(interaction) && log(interaction)
 	})
 	.login(Bun.env.TOKEN)
